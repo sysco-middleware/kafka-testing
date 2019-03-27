e5a80f720eb74aa6069a51ff639d5269a2a98148
zhenik <nikita.zhevnitskiy@gmail.com>
2019-03-27 22:34:58 +0100

    refactor no avro

--- streams-client/src/main/java/no/sysco/testing/kafka/streams/topology/StreamProcessing.java
+++ streams-client/src/main/java/no/sysco/testing/kafka/streams/topology/StreamProcessing.java
@@ -1,13 +1,10 @@
 package no.sysco.testing.kafka.streams.topology;
 
-import java.util.Optional;
-import java.util.Properties;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import no.sysco.testing.kafka.streams.utils.Tuple2;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
-import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
@@ -17,18 +14,7 @@ import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.kstream.ValueMapper;
 import org.apache.kafka.streams.state.KeyValueStore;
 
-public class StreamProcessing implements Runnable {
-  private final Properties properties;
-  private final String topicIn;
-  private final String topicOut;
-  private final KafkaStreams kafkaStreams;
-
-  public StreamProcessing(final Properties properties, final Tuple2<String, String> topics) {
-    this.properties = properties;
-    this.topicIn = topics._1;
-    this.topicOut = topics._2;
-    this.kafkaStreams = new KafkaStreams(topologyUpperCase(topics), properties);
-  }
+public class StreamProcessing {
 
   // stateless
   public static Topology topologyUpperCase(final Tuple2<String, String> topics) {
@@ -59,11 +45,5 @@ public class StreamProcessing implements Runnable {
     return streamsBuilder.build();
   }
 
-  @Override public void run() {
-    Optional.ofNullable(kafkaStreams).ifPresent(KafkaStreams::start);
-  }
 
-  public void close() {
-    Optional.ofNullable(kafkaStreams).ifPresent(KafkaStreams::close);
-  }
 }
