f1fa38a5d471698a6f5018cf625874e06bc88413
zhenik <nikita.zhevnitskiy@gmail.com>
2019-03-28 16:20:47 +0100

    add test topology with avro

--- streams-client/src/main/java/no/sysco/testing/kafka/streams/topology/StreamProcessingAvro.java
+++ streams-client/src/main/java/no/sysco/testing/kafka/streams/topology/StreamProcessingAvro.java
@@ -1,81 +1,49 @@
 package no.sysco.testing.kafka.streams.topology;
 
-
-import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
-import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
-import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
-import io.confluent.kafka.serializers.KafkaAvroDeserializer;
-import io.confluent.kafka.serializers.KafkaAvroSerializer;
-import io.confluent.kafka.streams.serdes.avro.SpecificAvroDeserializer;
-import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
-import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerializer;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Properties;
 import no.sysco.testing.kafka.streams.avro.Person;
 import no.sysco.testing.kafka.streams.utils.Tuple2;
-//import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.Printed;
+import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.kstream.ValueMapper;
+import org.apache.kafka.streams.state.KeyValueStore;
 
 public class StreamProcessingAvro {
 
   // stateless
-  public static Topology topologyUpperCase(final Tuple2<String, String> topics, final String schemaRegistryUrl) {
+  public static Topology topologyUpperCase(
+      final Tuple2<String, String> topics, final Serde<Person> personSerdes) {
     final var builder = new StreamsBuilder();
-
-    final var schema = Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
-
-    final Serde<Person> personSerdes = Serdes.serdeFrom(new SpecificAvroSerializer<Person>(), new SpecificAvroDeserializer<Person>());
-    //final Serde<Person> personSerdes = new SpecificAvroSerde();
-    personSerdes.configure(schema,false);
     builder.stream(topics._1, Consumed.with(Serdes.String(), personSerdes))
-        .peek((k, v) -> System.out.printf("%s %s %s\n", v.getId(), v.getName(), v.getLastname()))
-        .mapValues(person -> Person.newBuilder()
-            .setId(person.getId().toUpperCase())
-            .setName(person.getName().toUpperCase())
-            .setLastname(person.getLastname().toUpperCase())
-            .build()
-        )
+        // .peek((k, v) -> System.out.printf("%s %s %s\n", v.getId(), v.getName(), v.getLastname()))
+        .mapValues(
+            person ->
+                Person.newBuilder()
+                    .setId(person.getId().toUpperCase())
+                    .setName(person.getName().toUpperCase())
+                    .setLastname(person.getLastname().toUpperCase())
+                    .build())
         .to(topics._2, Produced.with(Serdes.String(), personSerdes));
-
     return builder.build();
   }
 
-  // stateless vers 2
-  public static Topology topologyUpperCase(final Tuple2<String, String> topics, final String schemaUrl, final SchemaRegistryClient registryClient) {
+  // stateful
+  public static Topology topologyCountUsersWithSameName(
+      final Tuple2<String, String> topics,
+      final Serde<Person> personSerdes,
+      final String storeName) {
+
     final var builder = new StreamsBuilder();
-    final Serde<Person> personSerde = new SpecificAvroSerde<Person>(registryClient);
-
-    final var schema = Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaUrl);
-    personSerde.configure(schema,false);
-
-    builder
-        .stream(topics._1, Consumed.with(Serdes.String(), personSerde))
-        .foreach((k, v) -> System.out.printf("%s %s %s\n", v.getId(), v.getName(), v.getLastname()));
+    builder.stream(topics._1, Consumed.with(Serdes.String(), personSerdes))
+        .groupBy((key, value) -> value.getName())
+        .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as(storeName))
+        .toStream()
+        .to(topics._2, Produced.with(Serdes.String(), Serdes.Long()));
 
     return builder.build();
   }
-
-  // stateless vers 3
-  //public static Topology topologyUpperCase(final Tuple2<String, String> topics, final String url, boolean flag) {
-  //  final var builder = new StreamsBuilder();
-  //
-  //
-  //  final Serde<Person> personSerde = new SpecificAvroSerde<Person>(registryClient);
-  //  builder
-  //      .stream(topics._1, Consumed.with(Serdes.String(), personSerde))
-  //      .foreach((k, v) -> System.out.printf("%s %s %s\n", v.getId(), v.getName(), v.getLastname()));
-  //
-  //  return builder.build();
-  //}
-
 }
--- streams-client/src/test/java/no/sysco/testing/kafka/streams/topology/StreamProcessingAvroTest.java
+++ streams-client/src/test/java/no/sysco/testing/kafka/streams/topology/StreamProcessingAvroTest.java
@@ -1,48 +1,42 @@
 package no.sysco.testing.kafka.streams.topology;
 
-import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
 import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
 import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import io.confluent.kafka.serializers.subject.TopicNameStrategy;
-import io.confluent.kafka.serializers.subject.TopicRecordNameStrategy;
-import io.confluent.kafka.streams.serdes.avro.SpecificAvroDeserializer;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
-import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerializer;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
 import no.sysco.testing.kafka.streams.avro.Person;
 import no.sysco.testing.kafka.streams.utils.Tuple2;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.TopologyTestDriver;
+import org.apache.kafka.streams.state.KeyValueStore;
 import org.apache.kafka.streams.test.ConsumerRecordFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class StreamProcessingAvroTest {
-  private TopologyTestDriver testDriver;
-  private Properties properties;
-  private final String topicIn    = "topic-in";
-  private final String topicOut   = "topic-out";
-  private final String schemaUrl  = "http://localhost:8081";
+  private final String topicIn = "topic-in";
+  private final String topicOut = "topic-out";
+  private final String schemaUrl = "http://localhost:8081";
   // http://localhost:8081/subjects/topic-in-value/versions/latest
   // only for TopicNameStrategy
   private final String mockedUrl = schemaUrl + "/subjects/" + topicIn + "-value/versions/latest";
-
+  private TopologyTestDriver testDriver;
+  private MockSchemaRegistryClient schemaRegistryClient;
+  private Properties properties;
 
   @Before
   public void start() {
@@ -50,9 +44,13 @@ public class StreamProcessingAvroTest {
     properties.put(StreamsConfig.CLIENT_ID_CONFIG, "client-id-test-1");
     properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "stream-id-test-5");
     properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:9922");
-    properties.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
-    properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+    properties.put(
+        AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+    properties.put(
+        StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
     properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
+
+    schemaRegistryClient = new MockSchemaRegistryClient();
   }
 
   @After
@@ -63,100 +61,91 @@ public class StreamProcessingAvroTest {
   }
 
   @Test
-  public void testTopologyAvro_statelessProcessors_Uppercase_approach1()
-      throws IOException, RestClientException {
+  public void testTopologyAvro_statelessProcessors() throws IOException, RestClientException {
 
-    // Arrange
-    MockSchemaRegistryClient schemaRegistryClient = new MockSchemaRegistryClient();
-    schemaRegistryClient.register(new TopicNameStrategy().subjectName(topicIn, false, Person.SCHEMA$), Person.SCHEMA$);
-    schemaRegistryClient.register(new TopicNameStrategy().subjectName(topicIn, true, Person.SCHEMA$), Person.SCHEMA$);
-
-    final Collection<String> allSubjects = schemaRegistryClient.getAllSubjects();
-    System.out.println("HERE: "+allSubjects);
-    System.out.println("HERE: "+schemaRegistryClient.getLatestSchemaMetadata("topic-in-value"));
-    System.out.println("HERE: "+schemaRegistryClient.getBySubjectAndId("topic-in-value", 1));
-
-    SpecificAvroSerde<Person> serde = new SpecificAvroSerde<>(schemaRegistryClient);
-
-    final Topology topology = StreamProcessingAvro.topologyUpperCase(new Tuple2<>(topicIn, topicOut), schemaUrl, schemaRegistryClient);
-
-    properties.put("schema.registry.url", "http://localhost:8081");
-    properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-    properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
-
+    /** Arrange */
+    // register schema in mock schema-registry
+    schemaRegistryClient.register(
+        new TopicNameStrategy().subjectName(topicIn, false, Person.SCHEMA$), Person.SCHEMA$);
+    // create serde with config to be able to connect to mock schema registry
+    final SpecificAvroSerde<Person> serde = new SpecificAvroSerde<>(schemaRegistryClient);
+    final var schema =
+        Collections.singletonMap(
+            AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
+            "wat-ever-url-anyway-it-is-mocked");
+    serde.configure(schema, false);
+    // get topology
+    final Topology topology =
+        StreamProcessingAvro.topologyUpperCase(new Tuple2<>(topicIn, topicOut), serde);
     testDriver = new TopologyTestDriver(topology, properties);
 
-    //final SpecificAvroSerializer<Person> personSpecificAvroSerializer =
-    //    new SpecificAvroSerializer<>();
-    //final var schema = Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaUrl);
-    //personSpecificAvroSerializer.configure(schema, false);
-    final ConsumerRecordFactory<String, Person> factory =
-        new ConsumerRecordFactory<>(topicIn, new StringSerializer(), serde.serializer());
-    final ConsumerRecord<byte[], byte[]> record1 = factory.create(topicIn, "1", Person.newBuilder()
-        .setId("id-1")
-        .setName("nikita")
-        .setLastname("zhevnitskiy")
-        .build()
-    );
+    final var factory =
+        new ConsumerRecordFactory<String, Person>(
+            topicIn, new StringSerializer(), serde.serializer());
+    final var inRecord1 =
+        factory.create(
+            topicIn,
+            "1",
+            Person.newBuilder().setId("id-1").setName("nikita").setLastname("zhevnitskiy").build());
+    final var inRecord2 =
+        factory.create(
+            topicIn,
+            "2",
+            Person.newBuilder().setId("id-2").setName("vitaly").setLastname("moscow").build());
 
-    testDriver.pipeInput(List.of(record1));
+    /** Act */
+    testDriver.pipeInput(List.of(inRecord1, inRecord2));
+    final ProducerRecord<String, Person> outRecord1 =
+        testDriver.readOutput(topicOut, new StringDeserializer(), serde.deserializer());
+    final ProducerRecord<String, Person> outRecord2 =
+        testDriver.readOutput(topicOut, new StringDeserializer(), serde.deserializer());
 
+    /** Assert */
+    assertEquals("ID-1", outRecord1.value().getId());
+    assertEquals("ID-2", outRecord2.value().getId());
+    assertEquals("moscow".toUpperCase(), outRecord2.value().getLastname());
   }
 
-  //@Test
-  //public void testTopologyAvro_statelessProcessors_Uppercase_approach2()
-  //    throws IOException, RestClientException {
-  //  // Arrange
-  //  MockSchemaRegistryClient schemaRegistryClient = new MockSchemaRegistryClient();
-  //  schemaRegistryClient.register("topic-in-value", Person.SCHEMA$);
-  //  schemaRegistryClient.register("topic-in-key", Person.SCHEMA$);
-  //
-  //  SpecificAvroSerde<Person> serde = new SpecificAvroSerde<>(schemaRegistryClient);
-  //
-  //  final Topology topology = StreamProcessingAvro.topologyUpperCase(new Tuple2<>(topicIn, topicOut), schemaRegistryClient);
-  //  testDriver = new TopologyTestDriver(topology, properties);
-  //
-  //
-  //  final SpecificAvroSerializer<Person> personSpecificAvroSerializer = new SpecificAvroSerializer<>();
-  //  final var schema = Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://dummy:1234");
-  //  personSpecificAvroSerializer.configure(schema, false);
-  //  final ConsumerRecordFactory<String, Person> factory =
-  //      new ConsumerRecordFactory<>(topicIn, new StringSerializer(), personSpecificAvroSerializer);
-  //
-  //
-  //  final ConsumerRecord<byte[], byte[]> record1 = factory.create(topicIn, "1", Person.newBuilder()
-  //      .setId("id-1")
-  //      .setName("nikita")
-  //      .setLastname("zhevnitskiy")
-  //      .build()
-  //  );
-  //
-  //}
+  @Test
+  public void testTopologyAvro_statefulProcessors() throws IOException, RestClientException {
+    /** Arrange */
+    final String storeName = "same-name";
+    // register schema in mock schema-registry
+    schemaRegistryClient.register(
+        new TopicNameStrategy().subjectName(topicIn, false, Person.SCHEMA$), Person.SCHEMA$);
+    // create serde with config to be able to connect to mock schema registry
+    final SpecificAvroSerde<Person> serde = new SpecificAvroSerde<>(schemaRegistryClient);
+    final var schema =
+        Collections.singletonMap(
+            AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
+            "wat-ever-url-anyway-it-is-mocked");
+    serde.configure(schema, false);
+    // get topology
+    final Topology topology =
+        StreamProcessingAvro.topologyCountUsersWithSameName(
+            new Tuple2<>(topicIn, topicOut), serde, storeName);
+    testDriver = new TopologyTestDriver(topology, properties);
 
-  //@Test
-  //public void serializationIssue() throws IOException, RestClientException {
-  //  //MockSchemaRegistryClient mockSchemaRegistryClient = new MockSchemaRegistryClient();
-  //  //final int register = mockSchemaRegistryClient.register("topic-in-value", Person.SCHEMA$);
-  //  //System.out.println("REGISTERED: "+register);
-  //  //final Topology topology = StreamProcessingAvro.topologyUpperCase(new Tuple2<>(topicIn, topicOut), mockSchemaRegistryClient);
-  //  //testDriver = new TopologyTestDriver(topology, properties);
-  //
-  //  final SpecificAvroSerializer<Person> personSpecificAvroSerializer = new SpecificAvroSerializer<>();
-  //  final var schema = Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaUrl);
-  //
-  //  personSpecificAvroSerializer.configure(schema, false);
-  //
-  //  final ConsumerRecordFactory<String, Person> factory =
-  //      new ConsumerRecordFactory<>(topicIn, new StringSerializer(), personSpecificAvroSerializer);
-  //
-  //  final Person person = Person.newBuilder()
-  //      .setId("id-1")
-  //      .setName("nikita")
-  //      .setLastname("zhevnitskiy")
-  //      .build();
-  //
-  //  final ConsumerRecord<byte[], byte[]> record = factory.create(topicIn, "1", person);
-  //
-  //}
+    final var factory =
+        new ConsumerRecordFactory<String, Person>(
+            topicIn, new StringSerializer(), serde.serializer());
+    final var inRecord1 =
+        factory.create(
+            topicIn,
+            "1",
+            Person.newBuilder().setId("id-1").setName("nikita").setLastname("zhevnitskiy").build());
+    final var inRecord2 =
+        factory.create(
+            topicIn,
+            "2",
+            Person.newBuilder().setId("id-2").setName("nikita").setLastname("moscow").build());
 
+    /** Act */
+    testDriver.pipeInput(List.of(inRecord1, inRecord2));
+    final KeyValueStore<String, Long> keyValueStore = testDriver.getKeyValueStore(storeName);
+    final Long amountOfRecordWithSameName = keyValueStore.get("nikita");
+
+    /** Assert */
+    assertEquals(Long.valueOf(2), amountOfRecordWithSameName);
+  }
 }
