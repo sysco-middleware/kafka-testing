317c51e70ae2c83e693295301068b5ff22577607
zhenik <nikita.zhevnitskiy@gmail.com>
2019-04-01 18:41:23 +0200

    example simpleProducer kafkaEmbeddedCluster

---
+++ consumer-producer-clients/readme.md
@@ -0,0 +1,10 @@
+# Description
+Module contains examples of testing Kafka Consumer/Producer APIs with: 
+- [X] mocked producer
+- [ ] embedded kafka
+
+## Additional Info
+* [scalatest-embedded-kafka](https://github.com/manub/scalatest-embedded-kafka) 
+* [kafka-it-embedded](https://github.com/asmaier/mini-kafka/blob/master/src/test/java/de/am/KafkaProducerIT.java) 
+* [embedded-single-node-kafka-cluster](https://www.confluent.io/blog/stream-processing-part-2-testing-your-streaming-application)
+* [issue-#26](https://github.com/confluentinc/kafka-streams-examples/issues/26) 
---
+++ consumer-producer-clients/src/main/java/no/sysco/testing/kafka/producer/SimpleProducer.java
@@ -0,0 +1,54 @@
+package no.sysco.testing.kafka.producer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import java.util.logging.Logger;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public final class SimpleProducer {
+  private final Properties properties;
+  private KafkaProducer<String, String> producer;
+  private String topicName;
+  private static final Logger logger = Logger.getLogger(SimpleProducer.class.getName());
+
+  public SimpleProducer(Properties properties, String topicName) {
+    this.properties = properties;
+    this.topicName = topicName;
+    this.producer = new KafkaProducer<String, String>(properties);
+  }
+
+  public void produceWithAsyncAck(String key, String value) {
+    // no key
+    final ProducerRecord<String, String> record = new ProducerRecord<>(topicName, key, value);
+    producer.send(
+        record,
+        (metadata, exception) -> {
+          if (exception == null) {
+            Map<String, Object> data = new HashMap<>();
+            data.put("topic", metadata.topic());
+            data.put("partition", metadata.partition());
+            data.put("offset", metadata.offset());
+            data.put("timestamp", metadata.timestamp());
+            logger.info(data.toString());
+          } else {
+            logger.log(Level.SEVERE, "Producer failed to send: {}", exception);
+            exception.printStackTrace();
+          }
+        });
+  }
+
+  public RecordMetadata produceWithSyncAck(String key, String value)
+      throws InterruptedException, ExecutionException, TimeoutException {
+    final ProducerRecord<String, String> record = new ProducerRecord<>(topicName, key, value);
+    return producer.send(record).get(1, TimeUnit.SECONDS);
+  }
+
+  public KafkaProducer<String, String> getProducer() { return producer; }
+}
