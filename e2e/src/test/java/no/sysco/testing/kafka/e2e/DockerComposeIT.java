eba4310f604d7204076cc49d8a77f79696315217
zhenik <nikita.zhevnitskiy@gmail.com>
2019-05-03 16:38:21 +0200

    add e2e containers approach

--- e2e/src/test/java/no/sysco/testing/kafka/e2e/DockerComposeIT.java
+++ e2e/src/test/java/no/sysco/testing/kafka/e2e/DockerComposeIT.java
@@ -6,6 +6,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class DataPipelineIT {
+public class DockerComposeIT {
 
 }
---
+++ e2e/src/test/java/no/sysco/testing/kafka/e2e/representation/MessageJsonRepresentation.java
@@ -0,0 +1,89 @@
+package no.sysco.testing.kafka.e2e.representation;
+
+public class MessageJsonRepresentation {
+
+  private String id;
+  private String from;
+  private String to;
+  private String text;
+
+  public MessageJsonRepresentation() {}
+
+  public MessageJsonRepresentation(String id, String from, String to, String text) {
+    this.id = id;
+    this.from = from;
+    this.to = to;
+    this.text = text;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getFrom() {
+    return from;
+  }
+
+  public void setFrom(String from) {
+    this.from = from;
+  }
+
+  public String getTo() {
+    return to;
+  }
+
+  public void setTo(String to) {
+    this.to = to;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  @Override
+  public String toString() {
+    return "MessageJsonRepresentation{"
+        + "id='"
+        + id
+        + '\''
+        + ", from='"
+        + from
+        + '\''
+        + ", to='"
+        + to
+        + '\''
+        + ", text='"
+        + text
+        + '\''
+        + '}';
+  }
+
+  public String json() {
+    return "{"
+        + "\"id\":"
+        + "\""
+        + id
+        + "\","
+        + "\"from\":"
+        + "\""
+        + from
+        + "\","
+        + "\"to\":"
+        + "\""
+        + to
+        + "\","
+        + "\"text\":"
+        + "\""
+        + text
+        + "\""
+        + "}";
+  }
+}
